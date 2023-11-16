ec069df3f5d8bb7100f4cb4ae0cf25b3c0a1c400
sesp13 <sespinosarte99@gmail.com>
2023-11-15 23:24:44 -0500

    instalar material y empezar a jugar con los componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Grid, Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Rifas</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Button variant="contained">Hola</Button>
+          Rifas
+        </Grid>
+        <Grid item xs={4}>
+          xs=4
+        </Grid>
+        <Grid item xs={4}>
+          xs=4
+        </Grid>
+        <Grid item xs={8}>
+          xs=8
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
