472c3172d0b7248446364ada1be37da6fb5a2590
sesp13 <sespinosarte99@gmail.com>
2024-01-18 23:26:20 -0500

    crate game log table

--- src/pages/GameLog/GameLogPage.tsx
+++ src/pages/GameLog/GameLogPage.tsx
@@ -1,9 +1,11 @@
-import { Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import { useAppSelector } from '../../hooks';
 import { GameLogTable } from './GameLogTable/GameLogtable';
 
 export const GameLogPage = () => {
-  const { rounds } = useAppSelector((state) => state.game);
+  const { rounds, players: storePlayers } = useAppSelector(
+    (state) => state.game
+  );
 
   return (
     <Grid container spacing={2}>
@@ -12,10 +14,29 @@ export const GameLogPage = () => {
           Registro de Juegos
         </Typography>
       </Grid>
-      <Grid item>
-        {rounds.map((round) => {
-          return <GameLogTable data={round} key={round.roundNumber} />;
-        })}
+      <Grid item xs={12}>
+        <Grid container spacing={2}>
+          {rounds.map(({ roundNumber, eventsPerPlayer, repartitorId }) => {
+            return (
+              <Grid item xs={12} key={roundNumber}>
+                <Container
+                  sx={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                  <Typography variant="h5" marginBottom={2}>
+                    Ronda {roundNumber}
+                  </Typography>
+                  <Typography variant="h5">
+                    Repartidor: {storePlayers[repartitorId].name}
+                  </Typography>
+                </Container>
+                <GameLogTable
+                  playerEvents={eventsPerPlayer}
+                  players={storePlayers}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/pages/GameLog/GameLogTable/GameLogtable.tsx
+++ src/pages/GameLog/GameLogTable/GameLogtable.tsx
@@ -1,17 +1,66 @@
-import { Grid, Typography } from '@mui/material';
-import { GameRound } from '../../../interfaces';
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { Player, PlayerEvent } from '../../../interfaces';
 
 interface GameLogTableProps {
-  data: GameRound;
+  playerEvents: Record<string, PlayerEvent>;
+  players: Record<string, Player>;
 }
 
 export const GameLogTable = (params: GameLogTableProps) => {
-  const { data } = params;
+  const { playerEvents, players: storePlayers } = params;
+  const playerKeys = Object.keys(playerEvents);
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h5">Ronda {data.roundNumber}</Typography>
-      </Grid>
-    </Grid>
+    <TableContainer component={Card} variant="outlined">
+      <Table sx={{ minWidth: 650 }} className="log-table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Puntaje inicial</TableCell>
+            <TableCell>Puntaje adquirido</TableCell>
+            <TableCell>Puntaje final</TableCell>
+            <TableCell>Voló en la ronda</TableCell>
+            <TableCell>Número de voladas</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {playerKeys.map((key) => {
+            return (
+              <TableRow key={key}>
+                <TableCell>
+                  <Typography>{storePlayers[key].name}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{playerEvents[key].startPoints}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{playerEvents[key].earnedPoints}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{playerEvents[key].endPoints}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>
+                    {playerEvents[key].isKickedOut ? 'Si' : 'No'}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{playerEvents[key].kickOuts}</Typography>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
